e7c29185218015af627158cb2d499bc6a468d576
Kiara Reyes <kiarareyesleiva@gmail.com>
2026-03-13 16:56:24 +0100

    PARAMETRES ACABAT

--- kiaraDJ/users/urls.py
+++ kiaraDJ/users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("profes", views.profes, name="professors"),
-    path("estudiants", views.estudiants, name="estudiants")
+    path("profes/", views.profes, name="professors"),
+    path("estudiants/", views.estudiants, name="estudiants"),
+    path("all/", views.all),
+    path("unUsuari/<int:pk>", views.unUsuari)
 ]
--- kiaraDJ/users/views.py
+++ kiaraDJ/users/views.py
@@ -1,68 +1,23 @@
 from django.shortcuts import render
+from .models import Person
 
 def estudiants(request):
-    llistaEstudiants = {
-        'estudiant 1':{
-            'id': 1,
-            'nombre': 'Sergio',
-            'apellido': 'Vaca',
-            'años': 20,
-            'pais': 'Navi'
-        },
-        'estudiant 2': {
-            'id': 2,
-            'nombre': 'Kiara',
-            'apellido': 'Reyes',
-            'años': 20,
-            'pais': 'Chipre'
-        },
-        'estudiant 3': {
-            'id': 3,
-            'nombre': 'Petunia',
-            'apellido': 'Reyes',
-            'años': 8,
-            'pais': 'España'
-        },
-        'estudiant 4': {
-            'id': 4,
-            'nombre': 'Ryan',
-            'apellido': 'Gosling',
-            'años': 1,
-            'pais': 'Chile'
-        }
-    }
-    msg = {'msg': llistaEstudiants}
+    llistaEst = Person.objects.filter(rol = "EST")
+    msg = {'msg': llistaEst}
     return render(request, 'main_page.html', msg)
+
 def profes(request):
-    llistaProfessors = {
-        'profe 1':{
-            'id': 1,
-            'nombre': 'Kike',
-            'apellido': 'Vaca',
-            'años': 90,
-            'pais': 'Guyana'
-        },
-        'profe 2': {
-            'id': 2,
-            'nombre': 'Gaby',
-            'apellido': 'Cardenas',
-            'años': 17,
-            'pais': 'Peru'
-        },
-        'profe 3': {
-            'id': 3,
-            'nombre': 'Diego',
-            'apellido': 'Torres',
-            'años': 99,
-            'pais': 'Surinam'
-        },
-        'profe 4': {
-            'id': 4,
-            'nombre': 'Francisco',
-            'apellido': 'Rojas',
-            'años': 38,
-            'pais': 'Arica'
-        }
-    }
-    msg = {'msg':llistaProfessors}
+    llistaPro = Person.objects.filter(rol = "PRO")
+    print(llistaPro)
+    msg = {'msg': llistaPro}
     return render (request, 'main_page.html', msg)
+
+def all(request):
+    getAll = Person.objects.all()
+    response = {"msg":getAll}
+    return render(request, 'main_page2.html', response)
+
+def unUsuari(request, pk):
+    getUserById = Person.objects.get(id=pk)
+    response = {"person": getUserById}
+    return render(request, 'main_page2.html', response)
